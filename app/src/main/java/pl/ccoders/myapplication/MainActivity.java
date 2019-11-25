aac92a707a3e2fd19e5170f023a24006cf838c83
macbookpro <bratukhinmaks@gmail.com>
2019-11-25 14:23:54 +0100

    initialisation

---
+++ app/src/main/java/pl/ccoders/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package pl.ccoders.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sayHello(View view) {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+    }
+}
